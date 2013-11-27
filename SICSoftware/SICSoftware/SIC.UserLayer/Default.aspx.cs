58738dbfb801ec0d27cc24a9d53faad842986b46
Juan Ruiz de Castilla <jujex_90@hotmail.com>
2013-11-27 15:46:54 -0500

    cambios en la aplicación

---
+++ SICSoftware/SICSoftware/SIC.UserLayer/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SIC.BusinessLayer;
+
+namespace SIC.UserLayer
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void btnIngresar_Click(object sender, System.EventArgs e)
+        {
+            SIC.EntityLayer.SIC_T_USUARIO mUsuario = null;
+
+            bool mExisteUsuario = false;
+            mExisteUsuario = new UsuarioBL().ObtenerUsuarioxNombre(txtUsuario.Text);
+
+            if (!mExisteUsuario)
+            {
+                lblMensaje.Text = "Usuario y/o Password es invalido";
+                return;
+            }
+            else
+            {
+                mUsuario = new UsuarioBL().Login(txtClave.Text);
+
+                if (mUsuario != null)
+                {
+                    Response.Redirect("Interfaces/Mantenimiento/frmRegCliente.aspx");
+                }
+                else
+                {
+                    lblMensaje.Text = "Usuario y/o Password es invalido";
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ SICSoftware/SICSoftware/SIC.UserLayer/Default.aspx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SIC.UserLayer {
+    
+    
+    public partial class Default {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control txtUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsuario;
+        
+        /// <summary>
+        /// Control txtClave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtClave;
+        
+        /// <summary>
+        /// Control lblMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensaje;
+        
+        /// <summary>
+        /// Control btnIngresar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnIngresar;
+    }
+}
